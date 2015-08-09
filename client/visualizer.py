a201f57d2041ec0772afd3a529b6ba94c095ecb0
boldx <boldx@rucsok.hu>
2015-08-09 17:21:58 +0200

    [enh] Moved the plotting in a different process, so it doesn't have to be in the main loop. Also the IPC is a bit more sophisticated.

--- client/visualizer.py
+++ client/visualizer.py
@@ -1,40 +1,98 @@
-import reflowcntrl
-import threading
-from matplotlib import pyplot as plot
+import multiprocessing
+import Queue
+import time
 
 class Visualizer:
 
-    def __init__(self, timebase, updateInterval, refData, updateCallback):
-        self._updateInterval = updateInterval
-        self._timebase = timebase
-        self._refData = refData
-        self._refDataLen = len(self._refData)
-        self._liveData = None
-        self._xdata = [i * self._timebase for i in range(self._refDataLen)]  # Linspace?
-        self._updateCallback = updateCallback
-
-        self._fig, self._axes = plot.subplots()
-        self._liveDataLine = None
-        plot.ion()
-
-        self._drawThread = threading.Thread(target=self._draw)
-        self._stopReq = threading.Event()
+    def __init__(self):
+        self._drawProcess = multiprocessing.Process(target=self._draw)
+        self._graphData = multiprocessing.Queue()
+        self._stopReq = multiprocessing.Event()
+        self._exitReq = multiprocessing.Event()
+        self._clearReq = multiprocessing.Event()
+        self._clearDone = multiprocessing.Event()
+        self._drawLive = multiprocessing.Event()
+        self._startTime = 0
+        self._processStarted = False
 
     def _draw(self):
-        while not self._stopReq.isSet():
-            self._liveData = self._updateCallback()
-            liveDataLen = len(self._liveData)
-            self._liveDataLine.set_ydata(self._liveData)
-            self._liveDataLine.set_xdata(self._xdata[:liveDataLen])
-            plot.draw()
-            plot.pause(self._updateInterval)
-
-    def start(self):
-        self._axes.plot(self._xdata, self._refData, "b-")
-        self._liveDataLine, = self._axes.plot([], [], "r-")
+        from matplotlib import pyplot as plot
+        fig, axes = plot.subplots()
+        fig.canvas.set_window_title('Reflow process')
+        fig.canvas.mpl_connect('close_event', self._onClosed)
+        plot.ion()
         plot.grid()
         plot.show()
-        self._draw()
+        while not self._exitReq.is_set():
+            refDataX, refDataY = self._graphData.get()
+            liveDataX = []
+            liveDataY = []
+            axes.plot(refDataX, refDataY, "b-")
+            liveDataLine, = axes.plot(liveDataX, liveDataY, "r-")
+            axes.set_xlim([0, refDataX[-1] * 1.1])
+            axes.set_ylim([min(refDataX) * 0.9, max(refDataY) * 1.1])
+            plot.draw()
+            while not self._stopReq.is_set():
+                try:
+                    while True:
+                        newPoint = self._graphData.get(block=False)
+                        liveDataX.append(newPoint[0])
+                        liveDataY.append(newPoint[1])
+                        liveDataLine.set_xdata(liveDataX)
+                        liveDataLine.set_ydata(liveDataY)
+                except Queue.Empty:
+                    pass
+                plot.pause(1)
+            while not self._clearReq.is_set():
+                plot.pause(1)
+            for line in axes.lines:
+                line.remove()
+            plot.draw()
+            self._clearQueue()
+            self._clearReq.clear()
+            self._clearDone.set()
+
+    def _onClosed(self, event):
+        pass
+
+    def _clearQueue(self):
+        while not self._graphData.empty():
+            try:
+                self._graphData.get(block=False)
+            except Queue.Empty:
+                continue
+
+    def _clearPlot(self):
+        self.disableLive()
+        self._clearReq.set()
+        self._clearDone.wait()
+        self._clearDone.clear()
+
+    def init(self, refData):
+        if self._processStarted:
+            self._clearPlot()
+        self._clearQueue()
+        self._graphData.put(refData)
+        if not self._processStarted:
+            self._processStarted = True
+            self._drawProcess.start()
+
+    def enableLive(self):
+        self._startTime = 0
+        self._stopReq.clear()
+        self._drawLive.set()
+        self._startTime = time.time()
+
+    def disableLive(self):
+        self._stopReq.set()
+        self._drawLive.clear()
+
+    def update(self, value):
+        elapsed = time.time() - self._startTime
+        if self._drawLive.is_set():
+            self._graphData.put((elapsed, value))
 
     def stop(self):
-        self._stopReq.set()
+        self.disableLive()
+        self._clearReq.set()
+        self._exitReq.set()
